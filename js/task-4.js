f2d65c514b08ee87d558ea989cf869fb8d7ac34c
Vitalii Radionov <Vetassikc@gmail.com>
2020-05-02 12:31:57 +0300

    add js-hw-3

---
+++ js/task-4.js
@@ -0,0 +1,27 @@
+const countTotalSalary = function (employees) {
+  let total = 0;
+  const values = Object.values(employees);
+
+  values.forEach(value => {
+    total += value;
+  });
+  return total;
+};
+
+console.log(countTotalSalary({}));
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+);
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+);
